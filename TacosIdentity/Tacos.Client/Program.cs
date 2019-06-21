4046cf3963c0e6849e6b8531b3e906299eb5e0d1
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2019-06-21 11:08:30 +0800

    add client and api project

---
+++ TacosIdentity/Tacos.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tacos.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
