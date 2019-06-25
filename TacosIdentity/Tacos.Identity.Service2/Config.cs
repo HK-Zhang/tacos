6da52f523ae4933e12412564e6c0764cb828176a
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2019-06-25 10:30:29 +0800

    asp.net core identity with Identity server4

---
+++ TacosIdentity/Tacos.Identity.Service2/Config.cs
@@ -0,0 +1,120 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Tacos.Identity.Service2
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+                        new Client
+        {
+            ClientId = "ro.client",
+            AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+            AllowedScopes = { "api1" }
+        },
+                               new Client
+        {
+            ClientId = "mvc",
+            ClientName = "MVC Client",
+
+            AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+            RequireConsent = false,
+
+    ClientSecrets =
+    {
+        new Secret("secret".Sha256())
+    },
+
+            RedirectUris = { "https://localhost:5005/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5005/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                "api1"  //Required for Hybrid flow
+            },
+             AllowOfflineAccess = true //Required for Hybrid flow
+        }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "1",
+            Username = "alice",
+            Password = "password",
+                        Claims = new []
+            {
+                new Claim("name", "Alice"),
+                new Claim("website", "https://alice.com")
+            }
+        },
+        new TestUser
+        {
+            SubjectId = "2",
+            Username = "bob",
+            Password = "password",
+                        Claims = new []
+            {
+                new Claim("name", "Alice"),
+                new Claim("website", "https://alice.com")
+            }
+        }
+    };
+        }
+    }
+}
---
+++ TacosIdentity/Tacos.Identity.Service2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tacos.Identity.Service2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
